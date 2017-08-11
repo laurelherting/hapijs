0fc4fd87abd0a3f4ed381ca4186a9c2077969285
Laurel Herting <laurel.herting@gmail.com>
2017-08-11 11:47:24 -0600

    hapi.js - POST and PUT request payloads

--- index.js
+++ index.js
@@ -3,24 +3,18 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
-server.register(require('vision'), () => {
-  
-  server.views({
-    engines: {
-      hbs: require('handlebars')
-    },
-    relativeTo: __dirname,
-    layout: true,
-    path: 'views'
-  })
-
   server.route({
-    method: 'GET',
-    path: '/{name?}',
+    method: ['POST', 'PUT'],
+    path: '/',
+    config: {
+      payload: {
+        output: 'data',
+        parse: false,
+        allow: 'application/json'
+      }
+    },
     handler: (request, reply) => {
-      reply.view('home', { name: request.params.name || 'world' })
+      reply(request.payload)
     }
   })
-
-  server.start(() => console.log(`Started at: ${server.info.uri}`))
-})
+server.start(() => {})
