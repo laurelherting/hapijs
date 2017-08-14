204b11fdbcd2d8f798c20d27c80c8784e79ea24d
Laurel Herting <laurel.herting@gmail.com>
2017-08-14 16:40:26 -0600

    Managing State with Cookies

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 'use strict'
 const Hapi = require('hapi')
-const Joi = require('joi')
 const server = new Hapi.Server()
 server.connection({ port: 8000 })
 
+server.state('hello', {
+  ttl: 60 * 60 * 1000,
+  isHttpOnly: true,
+  encoding: 'iron',
+  password: 'aabjgfnbve88ru8ureah8948agnknoaeg99873'
+})
+
 server.route({
-  method: ['POST', 'PUT'],
-  path: '/user/{id?}',
+  method: 'GET',
+  path: '/',
   config: {
-    validate: {
-      params: Joi.object({
-        id: Joi.number()
-      }),
-      query: Joi.object({
-        id: Joi.number()
-      })
-    },
     handler: (request, reply) => {
-      reply({
-        params: request.params,
-        query: request.query,
-        payload: request.payload
-      })
+      let hello = request.state.hello
+      reply(`Cookies! ${hello}`)
+       .state('hello', 'world')
     }
   }
 })
 
-server.start(() => {})
+server.start(() => console.log('Started at: ${server.info.uri}'))
